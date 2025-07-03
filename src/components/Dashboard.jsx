95a8324d6cf9e41b9d79c53a546d9325030ba048
aman-kazunov1ai <aman@kazunov1ai.com>
2025-07-03 21:20:12 +0530

    Water mark added

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -8,13 +8,17 @@ const Dashboard = () => {
 
   return (
     <div className="max-w-full md:max-w-6xl mx-auto px-4 sm:px-6 py-6 relative">
-      <div className="watermark">
-        <img src="/union.svg" alt="Watermark" />
-      </div>
       <AppHeader />
       <StatusCards activeTab={activeTab} setActiveTab={setActiveTab} />
       <div className="my-8"></div>
       <ApplicationsTable activeTab={activeTab} />
+
+      {/* Watermark */}
+      <img
+        src="/Union.svg"
+        alt="Watermark"
+        className="fixed bottom-0 right-0 w-64 sm:w-80 opacity-20 pointer-events-none select-none z-0"
+      />
     </div>
   );
 };
